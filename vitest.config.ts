96698da8c05b9057ae821ae524c998c4245bfdc5
Ken Snyder <ken@ken.net>
2022-12-09 10:47:23 -0800

    chore: removed "types" from ESM export block

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,17 +6,9 @@ import link from "./src/index";
 
 // used for testing, library code uses TSUP to build exports
 export default defineConfig({
-  test: {
-    dir: "test",
-    exclude: ["**/*.spec.ts"],
-    environment: "happy-dom",
-    api: {
-      host: "0.0.0.0",
-    },
-  },
   plugins: [
     Markdown({
-      builders: [ link() ],
+      builders: [link()],
     }),
     Vue({
       include: [/\.vue$/, /\.md$/],
